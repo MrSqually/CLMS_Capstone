1cbaba5a11c8e6ab8c74bd31d78418cee18c724b
Dean Cahill <dpcahill98@gmail.com>
2024-04-19 23:10:43 -0400

    structure

--- README.md
+++ README.md
@@ -1,13 +1,15 @@
-# Can You Say That Again?
-
-## Hallucinatory Variance in Large Language Models
+# Can You Say That Again? Hallucinatory Variance in Large Language Models
 
 This repository contains the source code, documentation, and writeup information
-for my Computational Linguistics MS Capstone. Detailed explanations of the motivation,
-experiments run, and
+for my Computational Linguistics MS Capstone.
 
-### Dataset
+## Overview / Pipeline 
 
-### Running Code
+## Dataset
 
-### Citation
+The dataset used for this capstone is Google's Natural Questions dataset. In order
+to properly control 
+
+## Running Code
+
+## Citation
--- src/topic_model/__init__.py
+++ src/topic_model/__init__.py
@@ -1,19 +1,93 @@
+#!/usr/bin/env python3
+
 # CLMS Capstone | Data Preprocessing & Topic Model
-# Copyright 2024 Dean Cahill  
+# Copyright 2024 Dean Cahill
 
 
 # ============================================================================|
-# Import Statements 
+# Import Statements
 # ============================================================================|
+from bertopic import BERTopic
+
+
+# Standard Imports
+import argparse
+import json
+import os
+
+# Typing Imports
+from typing import Iterator
+
 
 # ============================================================================|
-# Data Generator Pipeline
+# Data Iterator
+# TODO - parse individual document for relevant information
+# TODO - batch iterator for clustering pipeline
 # ============================================================================|
+def parse_document(doc: dict) -> list[str]:
+    """Parse an individual document
+
+    ## params
+    ## returns
+    STATUS: TODO
+    """
+
+    pass
+
+
+def batch_iterator(
+    data_dir: str | os.PathLike,
+    batch_size: int = 10,
+) -> Iterator[str]:
+    """Create a minibatch iterator for online topic modeling
+
+    ## params
+    ## returns
+    STATUS: TODO
+    """
+    pass
+
 
 # ============================================================================|
-# Topic Model 
+# Topic Model
 # ============================================================================|
+def run_topic_model(data_iter, **params) -> BERTopic:
+    """Primary topic model training/clustering loop
+
+    As NQ is a very large dataset with very large documents,
+    it makes sense to approach this as an Online clustering 
+    problem
+
+    ## params
+    - data_iter => batched data generator
+
+    ## returns
+    STATUS: TODO
+    """
+    pass
+
 
 # ============================================================================|
 # Main
 # ============================================================================|
+def parse_args() -> argparse.Namespace:
+    """Topic Model Argument Parser
+    STATUS: TODO
+    """
+    parser = argparse.ArgumentParser(
+        description="topic model for Natural Questions dataset"
+    )
+
+    parser.add_argument("--data_dir")
+    parser.add_argument("--config")
+
+    return parser.parse_args()
+
+
+def main(args: argparse.Namespace):
+    pass
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    main(args)
